857c110c18786c534e2b6c1cad23d468a848547a
ignacio <ignaciofranciscovigo@gmail.com>
2024-11-14 23:51:06 -0300

    Part 5a

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,20 +1,34 @@
 import { useState, useEffect } from "react";
-import notesService from "./Service/notes.js";
+import loginService from "./services/login.js";
+import notesService from "./services/notes.js";
 import Note from "./components/Note";
 import Notification from "./components/Notification.jsx";
 import Footer from "./components/Footer.jsx";
-
 const App = (props) => {
   const [notes, setNotes] = useState(null);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
+  // Get all notes
   useEffect(() => {
     notesService.getAll().then((allNotes) => {
       setNotes(allNotes);
     });
   }, []);
+
+  // Check if there s a logged in user in local storage
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      notesService.setToken(user.token);
+    }
+  }, []); // the empty array means to only run after the first render
 
   if (!notes) {
     return null;
@@ -41,7 +55,7 @@ const App = (props) => {
     });
   };
 
-  const handleChangeNewNote = (event) => {
+  const handleNoteChange = (event) => {
     setNewNote(event.target.value);
   };
 
@@ -78,10 +92,78 @@ const App = (props) => {
       });
   };
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("logging in with", username, password);
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      // Save token in local storage
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+
+      // Sets the token to the private variable of the notesService
+      notesService.setToken(user.token);
+
+      // Set new states
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  );
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}{" "}
+      <br></br>
       <button
         onClick={(event) => {
           setShowAll(!showAll);
@@ -102,15 +184,6 @@ const App = (props) => {
           ); // When creating a li component inside a ul componente, i can just pass the key in the ul list.
         })}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          type="text"
-          value={newNote}
-          placeholder="Type your note..."
-          onChange={handleChangeNewNote}
-        />
-        <button type="submit">save</button>
-      </form>
       <Footer />
     </div>
   );
